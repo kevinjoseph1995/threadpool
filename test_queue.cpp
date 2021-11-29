67d25900e82a91839094fe70a099316138794ff8
Kevin Joseph <kevinjoseph1995@gmail.com>
2021-11-29 06:46:58 +0000

    Some changes

--- test_queue.cpp
+++ test_queue.cpp
@@ -9,15 +9,20 @@
 #pragma clang diagnostic push
 #pragma ide diagnostic ignored "EndlessLoop"
 int main() {
+  std::atomic<int> count;
   std::mutex mutex;
   queue<int> my_queue;
-  auto add_element = [&my_queue](const int i) -> void { my_queue.push(i); };
-  auto remove_element = [&my_queue, &mutex]() -> void {
+  auto add_element = [&my_queue](const int i) -> void {
+    std::this_thread::sleep_for(std::chrono::microseconds(rand() % 1000));
+    my_queue.push(i);
+  };
+  auto remove_element = [&my_queue, &mutex, &count]() -> void {
     while (true) {
       auto ele = my_queue.wait_and_pop();
       std::lock_guard<std::mutex> lk(mutex);
       if (ele) {
-        std::cout << *ele << "\n";
+        count++;
+        std::cout << "count:" << count << " value: " << *ele << "\n";
       }
     }
   };
@@ -26,9 +31,10 @@ int main() {
   for (int i = 0; i < 1000; i++) {
     async_results.emplace_back(std::async(std::launch::async, add_element, i));
   }
-  async_results.emplace_back(std::async(std::launch::async, remove_element));
+  auto pop_thread_future = std::async(std::launch::async, remove_element);
   for (auto &res : async_results) {
     res.wait();
   }
+  pop_thread_future.wait();
 }
 #pragma clang diagnostic pop
